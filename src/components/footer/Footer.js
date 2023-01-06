3bc93345da9efcc0110944ae25a83811f10c0d54
Faisal Nazir <bhatfaisalfnb@gmail.com>
2023-01-06 17:43:57 +0530

    toastcont. commented

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="--flex-center --py2">
-      <p>All Rights Reserved. &copy; 2022</p>
+      <p>All Rights Reserved. &copy; 2023</p>
     </div>
   );
 };
