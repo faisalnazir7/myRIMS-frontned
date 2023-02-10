f235e46e97bde26354531cc0194f5b88f091e48f
Faisal Nazir <bhatfaisalfnb@gmail.com>
2023-02-11 00:22:11 +0530

    Home page...

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -37,10 +37,10 @@ const Home = () => {
       {/* HERO SECTION */}
       <section className="container hero">
         <div className="hero-text">
-          <h2>Inventory {"&"} Stock Management Solution</h2>
+          <h2>My Inventory</h2>
           <p>
-            Inventory system to control and manage proucts in the warehouse in
-            real timeand integrated to make it easier to develop your business.
+            Inventory system to control and manage products in the warehouse in
+            real time and integrated to make it easier to manage your business.
           </p>
           <div className="hero-buttons">
             <button className="--btn --btn-secondary">
@@ -50,7 +50,7 @@ const Home = () => {
         </div>
 
         <div className="hero-image">
-          <img src={heroImg} alt="Inventory" />
+          <img src={heroImg} alt="Inventory" width="267" />
         </div>
       </section>
     </div>
