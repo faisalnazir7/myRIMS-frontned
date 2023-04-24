7b4e10c7af82302b3574a85e8da4014f1c2686f4
Faisal Nazir <bhatfaisalfnb@gmail.com>
2023-04-24 22:11:08 +0530

    header update

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -20,7 +20,7 @@ const Header = () => {
       <div className="--flex-between">
         <h3>
           <span className="--fw-thin">Welcome, </span>
-          <span className="--color-danger">{name}</span>
+          <span className="--color-primary">{name}</span>
         </h3>
         <button onClick={logout} className="--btn --btn-danger">
           Logout
